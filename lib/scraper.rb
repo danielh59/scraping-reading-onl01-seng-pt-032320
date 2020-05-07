e98ea1280366b9de3c4b291148516e07a95dd9a3
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-05-07 10:32:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,6 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 courses = doc.css("#id-3c239880-48cf-5ce7-84d0-578546d87586 .inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
 
-courses.each |course|
+courses.each |course| do
 puts course.text.strip
 end
